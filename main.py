e15d483adf2f09e65db109cc47d6bbeb9cfcfda5
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-08-10 16:47:41 +1000

    gently exiting upon browser close

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import time
 import json
-
 import obsws_python as obs
 from selenium import webdriver
+import signal
 
-ws = obs.ReqClient(host='192.168.1.92', port=4444)
+ws = obs.ReqClient(host='localhost', port=4444)
 
 # Chromium Webdriver
 
 options = webdriver.ChromeOptions()
 options.add_argument('--enable-logging')
-
 driver = webdriver.Chrome(options=options)
 
 # Adding cookies to check if previously logged in
-
+driver.get('https://discord.com')
 cookie_file = 'discord_cookies.json'
 
 try:
@@ -27,11 +26,13 @@ try:
 
         driver.get('https://discord.com/app')
 
+        # Check if the login was successful; if not, ask the user to login manually
+        if "login" in driver.current_url:
+            raise FileNotFoundError
+
 except FileNotFoundError:
     print("No cookies file found. Please log in to Discord and navigate to a server or chat. Waiting for 30 seconds...")
     time.sleep(15)
-
-    print("Wait finished. Saving cookies...")
 
     cookies = driver.get_cookies()
     with open(cookie_file, 'w') as file:
@@ -39,33 +40,12 @@ except FileNotFoundError:
 
     print("Cookies saved.")
 
-
-# Open the website
-
-driver.get('https://discord.com/login')
-
-print("Please log in to Discord and navigate to a server or chat. Waiting for 30 seconds...")
-time.sleep(10)
-
-print("Wait finished. Starting to check if popout window is detected...")
-
-# Simplifying to just check if the popout window is detected, instead of active
 initial_window_count = len(driver.window_handles)
 
+print("Waiting for window popup to start recording...")
 
 def is_popout_open():
     return len(driver.window_handles) > initial_window_count
-
-
-popout_opened = False
-
-# Wait for the popout to open
-while not popout_opened:
-    if is_popout_open():
-        print("Popout detected!")
-        popout_opened = True
-    time.sleep(5)
-
 
 def start_recording():
     print("Starting recording...")
@@ -76,10 +56,29 @@ def stop_recording():
     ws.stop_record()
 
 
-if popout_opened:
-    start_recording()
-    # Wait for the popout to close
-    while is_popout_open():
-        time.sleep(5)
-    print("Popout closed. Stopping recording...")
+def gentle_exit(signum, frame):
+    print("Exiting...")
     stop_recording()
+    # Save cookies
+    cookies = driver.get_cookies()
+    with open(cookie_file, 'w') as file:
+        json.dump(cookies, file)
+    print("Cookies saved.")
+    driver.quit()
+    exit(0)
+
+signal.signal(signal.SIGTERM, gentle_exit)
+signal.signal(signal.SIGINT, gentle_exit)
+
+
+# Continuously check for popouts
+while True:
+    if is_popout_open():
+        print("Popout detected!")
+        start_recording()
+        # Wait for the popout to close
+        while is_popout_open():
+            time.sleep(5)
+        print("Popout closed. Stopping recording...")
+        stop_recording()
+    time.sleep(10)
