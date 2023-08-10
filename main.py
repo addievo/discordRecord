9e774e5458919178bdb4dff47d100d222c81ab41
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-08-10 16:21:38 +1000

    storing user session

--- main.py
+++ main.py
@@ -1,53 +1,46 @@
 import time
-import json
-
+import pickle
 import obsws_python as obs
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-ws = obs.ReqClient(host='192.168.1.92', port=4444)  # No password as you mentioned
 
-# Chromium Webdriver
+ws = obs.ReqClient(host='192.168.1.92', port=4444)
 
-options = webdriver.ChromeOptions()
-options.add_argument('--enable-logging')
 
-driver = webdriver.Chrome(options=options)
 
-# Adding cookies to check if previously logged in
+chrome_options = Options()
+chrome_options.add_argument("user-data-dir=selenium")
 
-cookie_file = 'discord_cookies.json'
 
-try:
-    with open(cookie_file, 'r') as file:
-        cookies = json.load(file)
-        for cookie in cookies:
-            driver.add_cookie(cookie)
-
-        # Refresh the page to apply the cookies and navigate to app
-
-        driver.get('https://discord.com/app')
-
-except FileNotFoundError:
-    print("No cookies file found. Please log in to Discord and navigate to a server or chat. Waiting for 30 seconds...")
-    time.sleep(30)
-
-    print("Wait finished. Saving cookies...")
-
-    cookies = driver.get_cookies()
-    with open(cookie_file, 'w') as file:
-        json.dump(cookies, file)
-
-    print("Cookies saved.")
-
+driver = webdriver.Chrome(options=chrome_options)
 
 # Open the website
+driver.get('https://discord.com/')
 
-driver.get('https://discord.com/login')
 
-print("Please log in to Discord and navigate to a server or chat. Waiting for 30 seconds...")
-time.sleep(10)
+# Check if cookies file exists and load cookies if it does
+cookie_file = 'discord_cookies.json'
 
-print("Wait finished. Starting to check if popout window is detected...")
+
+try:
+    cookies = pickle.load(open("cookies.pkl", "rb"))
+    # Set the cookies
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+    # Refresh or navigate to the app page after setting cookies
+    driver.get('https://discord.com/app')
+except FileNotFoundError:
+    driver.get('https://discord.com/login')
+    print("Please log in to Discord. Waiting for 30 seconds...")
+    time.sleep(15)
+    # Save the cookies after logging in
+    cookies = driver.get_cookies()
+    pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+    print("Cookies saved to cookies.pkl")
+
+print("Starting to check if popout window is detected...")
 
 # Simplifying to just check if the popout window is detected, instead of active
 initial_window_count = len(driver.window_handles)
@@ -55,16 +48,6 @@ initial_window_count = len(driver.window_handles)
 
 def is_popout_open():
     return len(driver.window_handles) > initial_window_count
-
-
-popout_opened = False
-
-# Wait for the popout to open
-while not popout_opened:
-    if is_popout_open():
-        print("Popout detected!")
-        popout_opened = True
-    time.sleep(5)
 
 
 def start_recording():
@@ -76,10 +59,14 @@ def stop_recording():
     ws.stop_record()
 
 
-if popout_opened:
-    start_recording()
-    # Wait for the popout to close
-    while is_popout_open():
-        time.sleep(5)
-    print("Popout closed. Stopping recording...")
-    stop_recording()
+# Continuously check for popouts
+while True:
+    if is_popout_open():
+        print("Popout detected!")
+        start_recording()
+        # Wait for the popout to close
+        while is_popout_open():
+            time.sleep(5)
+        print("Popout closed. Stopping recording...")
+        stop_recording()
+    time.sleep(10)
